4c806efd3b754e05a08beb021998605de59e29a8
TonyHeiser <121868476+TonyHeiser@users.noreply.github.com>
2023-06-17 09:06:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var cardList = document.querySelector('.products');
-console.log(cardList.children);
+// var cardList = document.querySelector('.items');
+// console.log(cardList.children);
+
+// var listItem = document.createElement("div");
+// listItem.classList.add("item");
+// cardList.appendChild(listItem);
+// console.log(cardList.children);
